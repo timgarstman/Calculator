add7dbc84a530054771dd3d5adc933f88ad2701e
timgarstman <timgarstman@hotmail.com>
2015-12-02 21:35:46 +0100

    Assignment 1 & 2

---
+++ CalculatorBrain.swift
@@ -0,0 +1,116 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Tim Garstman on 28-11-15.
+//  Copyright © 2015 Tim Garstman. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    private enum Op: CustomStringConvertible //printable
+    {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get{
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+    
+    private var opStack = [Op]()
+    
+    private var knownOps = [String: Op]()
+    
+    init(){
+        func learnOp(op: Op) {
+            knownOps[op.description] = op
+        }
+        
+        learnOp(Op.BinaryOperation("x", *))
+        knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+        knownOps["sin"] = Op.UnaryOperation("sin", sin)
+        knownOps["cos"] = Op.UnaryOperation("cos", cos)
+        //knownOps["π"] = Op.BinaryOperation("π", M_PI)
+        //BinaryOperation en UnaryOperation geven bij pi beiden een foutmelding
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op])
+    {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op{
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    //hier var variableValues toevoegen
+    func evaluate() -> Double? {
+        if var variableValues != nil{
+            let (result, remainder) = evaluate(opStack)
+            print("\(opStack) = \(result) with \(remainder) left over")
+            return result
+        } else {
+            return nil
+        }
+    }
+    
+    
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+        
+    }
+    
+    //functie om de remainingOps te verwijderen
+    func clear(){
+        opStack.removeAll()
+    }
+    
+    //API, niet helemaal duidelijk wat ik bij deze opdracht (5) moest doen.
+    func pushOperand(symbol: String) -> Double?
+    var variableValues: Dictionary<String, Double>
+    
+    
+    
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Nico Garstman on 24-11-15.
+//  Copyright © 2015 Tim Garstman. All rights reserved.
+//
+
+// Een type kan twee values hebben:
+// optie 1: Not set = nil
+// optie 2: Something = String
+// String? = Optional string
+// String! = een string unwrappen, LET OP, als de string nil is dan crasht het programma
+
+
+//All properties in swift has to be initialized
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    //Display van de rekenmachine
+    @IBOutlet weak var display: UILabel!
+    
+    //Geschiedenis van de rekenmachine
+    @IBOutlet weak var history: UILabel!                    //history
+
+    
+    //Variable instellen voor het typen van de gebruiker
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    
+    //link naar CalculatorBrian
+    var brain = CalculatorBrain()
+    
+    
+    //Toevoegen van een cijfer
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+            history.text = history.text! + digit            //history
+        } else {
+            display.text = digit
+            history.text = digit                            //history
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    
+    //Uitvoeren van een operator
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+                history.text = history.text! + operation    //history
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    
+    //Klik op enter
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+    
+    
+    //Toevoegen van een decimaal
+    @IBAction func dot(sender: UIButton) {
+        if (display.text!.rangeOfString(".") != nil) {
+            return
+        } else{
+            display.text = display.text! + "."
+        }
+    }
+    
+    //Rekenmachine leeg maken
+    @IBAction func Clear(sender: UIButton) {
+        displayValue = 0
+        brain.clear()
+        enter()
+    }
+    
+    
+    
+    //Het getal kan getallen na de decimaal bevatten = double
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
+    //vanaf assignment 2 opdracht 7 wist ik niet hoe ik dit moest aanpakken.
+
+
+}
